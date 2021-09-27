2983917a02b4dba2eca86548623c153447a6f4c3
Dumar Yeksel Basto Moreno - Ceiba Software <dumar.basto@ceiba.com.co>
2021-09-27 16:39:50 -0500

    Se agrega nueva excepción y se cambian los mensajes, este ajuste es usado en el anterior commit

--- app/src/main/java/com/dybm27/patternfacade/home/model/ModelException.kt
+++ app/src/main/java/com/dybm27/patternfacade/home/model/ModelException.kt
@@ -4,20 +4,21 @@ import java.lang.RuntimeException
 
 class ModelException(override val message: String) : RuntimeException() {
     companion object Messages {
-        const val YOU_ARE_NOT_AFFILIATED = "No te encuentras afiliado."
+        const val YOU_ARE_NOT_AFFILIATED = "No extiste un paciente afiliado con ese documento."
         const val SPECIALIST_NOT_AVAILABLE =
             "El especialista no tiene disponibilidad en esa fecha."
+        const val THEY_ALREADY_HAVE_A_REGISTERED_APPOINTMENT_FOR_THIS_DATE =
+            "El paciente ya tiene una cita agendada para esa fecha."
         const val THEY_ALREADY_HAVE_A_REGISTERED_APPOINTMENT_WITH_THE_SPECIALIST =
-            "Ya tienen cita registrada con el especialista."
+            "El paciente ya tiene una cita agendada con el especialista."
         const val REGISTERED_APPOINTMENT =
-            "Cita registrada."
+            "Cita agendada."
         const val INVALID_DATE =
-            "Favor revisar la fecha.\n" +
-                    "El horario de atención es de lunes a viernes\n"
+            "El horario de atención es de lunes a viernes."
         const val INVALID_TIME =
-            "Favor revisar la hora.\n" +
-                    "El horario de atención es de 8am a 12pm y de 2pm a 6pm.\n" +
-                    "Y se programa cada 30 min "
+            "El horario de atención es de 8am a 12pm y de 2pm a 6pm."
+        const val INVALID_TIME2 =
+            "La cita se debe agendar cada 30 minutos."
         const val INVALID_ANTICIPATION_DAYS =
             "La cita se debe agendar por lo menos con 3 días de anticipación."
     }
